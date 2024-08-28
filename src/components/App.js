63c1f9e320aa7817712bb4364eb0681ba299779e
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-28 17:27:17 +0000

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
 
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// src/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+      
+    </div>
+  );
+};
+
+export default Home;
